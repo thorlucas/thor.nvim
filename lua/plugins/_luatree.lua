aa3f9038aac1f70132586b237685b98d86ca4cda
Thor Correia <thor.lucas@icloud.com>
2021-01-12 23:18:18 -0800

    Updated LuaTree to NvimTree

--- lua/plugins/_luatree.lua
+++ lua/plugins/_luatree.lua
@@ -1,9 +1,9 @@
 vim.cmd [[packadd nvim-tree.lua]]
 
-vim.g.lua_tree_auto_open = 1
-vim.g.lua_tree_ignore = { '.git', 'node_modules', '.cache', 'dist', '.DS_Store' }
-vim.g.lua_tree_tab_open = 1
-vim.g.lua_tree_git_hl = 1
-vim.g.lua_tree_show_icons = { git = 0, folders = 1, files = 1 }
+vim.g.nvim_tree_auto_open = 1
+vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache', 'dist', '.DS_Store' }
+vim.g.nvim_tree_tab_open = 1
+vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_show_icons = { git = 0, folders = 1, files = 1 }
 
-require('util.map').keymap('n', '<C-n>', ':LuaTreeToggle<CR>')
+require('util.map').keymap('n', '<C-n>', ':NvimTreeToggle<CR>')
