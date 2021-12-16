2eff9733ba1a61af45aee51b2e397dc2b4443802
Thor Correia <thor.lucas@berkeley.edu>
2021-12-16 02:57:28 -0800

    simple config listeners

--- lua/config.lua
+++ lua/config.lua
@@ -1,8 +1,9 @@
 local M = {}
-local config = {}
+local _listeners = {}
 
-_G[config] = _G[config] or {
+_G._config = _G._config or {
 	debug = false,
+	_listeners = {},
 }
 
 local function merge(t1, t2)
@@ -22,48 +23,110 @@ local function merge(t1, t2)
 end
 
 local parse_path_string = function(path)
-	return vim.fn.split(path, [[\.]])
+	if path == nil then
+		return {}
+	elseif type(path) == 'string' then
+		return vim.fn.split(path, [[\.]])
+	elseif type(path) == 'table' then
+		return path
+	else
+		vim.notify("Error parsing path: "..vim.inspect(path))
+	end
 end
+
+local notify_at_level = function(ls, value, path)
+	for _, l in ipairs(ls) do
+		l(value, path)
+	end
+end
+
+M.notify = function(path)
+	path = parse_path_string(path)
+	local t = _G._config
+	local l = t._listeners
+	local p = {}
+
+	for _, key in ipairs(path) do
+		notify_at_level(l, t, p)
+
+		l = l[key]
+		t = t[key]
+		table.insert(p, key)
+
+		if l == nil then
+			return
+		elseif t == nil then
+			break
+		end
+	end
+	
+	notify_at_level(l, t, p)
+end
+
+local schedule_notify = vim.schedule_wrap(M.notify)
 
 M.merge = function(values)
-	merge(_G[config], values)
-	print("shoud reload")
+	merge(_G._config, values)
+	schedule_notify()
 end
 
-M.get = function(path_string)
-	local path = path_string and parse_path_string(path_string) or {}
-	local table = _G[config]
+M.get = function(path)
+	path = parse_path_string(path)
+	local t = _G._config
 	for _, key in ipairs(path) do
-		if table == nil then
+		if t == nil then
 			return nil
 		end
-		table = table[key]
+		t = t[key]
 	end
-	return table
+	return t
 end
 
-M.set = function(path_string, value)
-	local path = parse_path_string(path_string)
-	local table = _G[config]
+M.set = function(path, value)
+	path = parse_path_string(path)
+	local t = _G._config
 	for index, key in ipairs(path) do
 		if index == #path then
-			if table[key] == nil then
-				table[key] = value
-			elseif type(table[key]) == 'table' and type(value) == 'table' then
-				merge(table[key], value)
+			if t[key] == nil then
+				t[key] = value
+			elseif type(t[key]) == 'table' and type(value) == 'table' then
+				merge(t[key], value)
 			else
-				table[key] = value
+				t[key] = value
 			end
-		elseif table[key] == nil then
-			table[key] = {}
+		elseif t[key] == nil then
+			t[key] = {}
 		end
-		table = table[key]
+		t = t[key]
 	end
-	return table
+	schedule_notify(path)
+	return t
+end
+
+M.attach = function(path, callback)
+	path = parse_path_string(path)
+	local ls = _G._config._listeners
+
+	for index, key in ipairs(path) do
+		if ls[key] == nil then
+			ls[key] = {}
+		end
+
+		if index == #path then
+			table.insert(ls[key], callback)
+		else
+			ls = ls[key]
+		end
+	end
+	
+	local current = M.get(path)
+	if current ~= nil then
+		callback(current, path)
+	end
 end
 
 setmetatable(M, {
-	__index = _G[config],
+	__index = _G._config,
 	__call = function(this, values)
 		this.merge(values)
 		return this
